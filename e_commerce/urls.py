48fe46a7e4cc44520920008e8a1d5bf87b8e5953
jefferson <jeff_guns@hotmail.com>
2022-06-26 03:39:24 -0300

    modified:   e_commerce/urls.py, new file:   template/about/view.html, new file:   template/contact/view.html and new file:   template/contact/view.html

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-from .views import home_page  about_page, contact_page
+from .views import home_page,  about_page, contact_page
 
 
 urlpatterns = [
